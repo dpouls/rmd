9f619b99ee56991f8c460cd21648dc868887c810
Dallin <dallinpoulson@gmail.com>
2021-01-11 21:21:43 -0700

    starting with framer motion on gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,21 +1,30 @@
-import React, {useEffect} from 'react'
-import useFirestore from '../../hooks/useFirestore';
-import { withRouter } from 'react-router-dom'
-import './Gallery.scss'
+import React, { useEffect } from "react";
+import useFirestore from "../../hooks/useFirestore";
+import { withRouter } from "react-router-dom";
+import "./Gallery.scss";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
-    const {docs} = useFirestore('images');
-    return (
-        <div className='gallery-page'>
-            {docs && docs.map((link,i) => {
-                return(
-                    <div key={link.id} className={`grid-item item-${i}`}>
-                        <img className='grid-img' src={link.url} alt='display or pedestal'/>
-                    </div> 
-                )
-            })}
-        </div>
-    )
-}
+  const { docs } = useFirestore("images");
+  return (
+    <div className="gallery-page">
+      {docs &&
+        docs.map((link, i) => {
+          return (
+            <motion.div layout key={link.id} className={`grid-item item-${i}`}>
+              <motion.img
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.5 }}
+                className="grid-img"
+                src={link.url}
+                alt="display or pedestal"
+              />
+            </motion.div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default withRouter(Gallery);
